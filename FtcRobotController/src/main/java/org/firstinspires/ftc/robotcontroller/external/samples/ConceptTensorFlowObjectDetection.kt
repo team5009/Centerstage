c257bbca4ce24a2f9d98921e077b15f03d6b12b2
wueyisfnfd <nathaniel.ah.labelle@gmail.com>
2023-09-30 14:45:58 -0600

    Converted ConceptTensorFlowObjectDetection.java to Kotlin

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/ConceptTensorFlowObjectDetection.kt
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/ConceptTensorFlowObjectDetection.kt
@@ -26,19 +26,15 @@
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+package org.firstinspires.ftc.robotcontroller.external.samples
 
-package org.firstinspires.ftc.robotcontroller.external.samples;
-
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
-import org.firstinspires.ftc.vision.VisionPortal;
-import org.firstinspires.ftc.vision.tfod.TfodProcessor;
-
-import java.util.List;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName
+import org.firstinspires.ftc.vision.VisionPortal
+import org.firstinspires.ftc.vision.tfod.TfodProcessor
 
 /*
  * This OpMode illustrates the basics of TensorFlow Object Detection,
@@ -49,85 +45,72 @@ import java.util.List;
  */
 @TeleOp(name = "Concept: TensorFlow Object Detection", group = "Concept")
 @Disabled
-public class ConceptTensorFlowObjectDetection extends LinearOpMode {
-
-    private static final boolean USE_WEBCAM = true;  // true for webcam, false for phone camera
-
+class ConceptTensorFlowObjectDetection : LinearOpMode() {
     /**
      * The variable to store our instance of the TensorFlow Object Detection processor.
      */
-    private TfodProcessor tfod;
+    private var tfod: TfodProcessor? = null
 
     /**
      * The variable to store our instance of the vision portal.
      */
-    private VisionPortal visionPortal;
-
-    @Override
-    public void runOpMode() {
-
-        initTfod();
+    private var visionPortal: VisionPortal? = null
+    override fun runOpMode() {
+        initTfod()
 
         // Wait for the DS start button to be touched.
-        telemetry.addData("DS preview on/off", "3 dots, Camera Stream");
-        telemetry.addData(">", "Touch Play to start OpMode");
-        telemetry.update();
-        waitForStart();
-
+        telemetry.addData("DS preview on/off", "3 dots, Camera Stream")
+        telemetry.addData(">", "Touch Play to start OpMode")
+        telemetry.update()
+        waitForStart()
         if (opModeIsActive()) {
             while (opModeIsActive()) {
-
-                telemetryTfod();
+                telemetryTfod()
 
                 // Push telemetry to the Driver Station.
-                telemetry.update();
+                telemetry.update()
 
                 // Save CPU resources; can resume streaming when needed.
                 if (gamepad1.dpad_down) {
-                    visionPortal.stopStreaming();
+                    visionPortal!!.stopStreaming()
                 } else if (gamepad1.dpad_up) {
-                    visionPortal.resumeStreaming();
+                    visionPortal!!.resumeStreaming()
                 }
 
                 // Share the CPU.
-                sleep(20);
+                sleep(20)
             }
         }
 
         // Save more CPU resources when camera is no longer needed.
-        visionPortal.close();
-
-    }   // end runOpMode()
+        visionPortal!!.close()
+    } // end runOpMode()
 
     /**
      * Initialize the TensorFlow Object Detection processor.
      */
-    private void initTfod() {
+    private fun initTfod() {
 
         // Create the TensorFlow processor by using a builder.
-        tfod = new TfodProcessor.Builder()
-
-            // Use setModelAssetName() if the TF Model is built in as an asset.
-            // Use setModelFileName() if you have downloaded a custom team model to the Robot Controller.
-            //.setModelAssetName(TFOD_MODEL_ASSET)
-            //.setModelFileName(TFOD_MODEL_FILE)
-
-            //.setModelLabels(LABELS)
-            //.setIsModelTensorFlow2(true)
-            //.setIsModelQuantized(true)
-            //.setModelInputSize(300)
-            //.setModelAspectRatio(16.0 / 9.0)
-
-            .build();
+        tfod = TfodProcessor.Builder() // Use setModelAssetName() if the TF Model is built in as an asset.
+                // Use setModelFileName() if you have downloaded a custom team model to the Robot Controller.
+                //.setModelAssetName(TFOD_MODEL_ASSET)
+                //.setModelFileName(TFOD_MODEL_FILE)
+                //.setModelLabels(LABELS)
+                //.setIsModelTensorFlow2(true)
+                //.setIsModelQuantized(true)
+                //.setModelInputSize(300)
+                //.setModelAspectRatio(16.0 / 9.0)
+                .build()
 
         // Create the vision portal by using a builder.
-        VisionPortal.Builder builder = new VisionPortal.Builder();
+        val builder = VisionPortal.Builder()
 
         // Set the camera (webcam vs. built-in RC phone camera).
         if (USE_WEBCAM) {
-            builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"));
+            builder.setCamera(hardwareMap.get(WebcamName::class.java, "Webcam 1"))
         } else {
-            builder.setCamera(BuiltinCameraDirection.BACK);
+            builder.setCamera(BuiltinCameraDirection.BACK)
         }
 
         // Choose a camera resolution. Not all cameras support all resolutions.
@@ -145,38 +128,37 @@ public class ConceptTensorFlowObjectDetection extends LinearOpMode {
         //builder.setAutoStopLiveView(false);
 
         // Set and enable the processor.
-        builder.addProcessor(tfod);
+        builder.addProcessor(tfod)
 
         // Build the Vision Portal, using the above settings.
-        visionPortal = builder.build();
+        visionPortal = builder.build()
 
         // Set confidence threshold for TFOD recognitions, at any time.
         //tfod.setMinResultConfidence(0.75f);
 
         // Disable or re-enable the TFOD processor at any time.
         //visionPortal.setProcessorEnabled(tfod, true);
-
-    }   // end method initTfod()
+    } // end method initTfod()
 
     /**
      * Add telemetry about TensorFlow Object Detection (TFOD) recognitions.
      */
-    private void telemetryTfod() {
-
-        List<Recognition> currentRecognitions = tfod.getRecognitions();
-        telemetry.addData("# Objects Detected", currentRecognitions.size());
+    private fun telemetryTfod() {
+        val currentRecognitions = tfod!!.recognitions
+        telemetry.addData("# Objects Detected", currentRecognitions.size)
 
         // Step through the list of recognitions and display info for each one.
-        for (Recognition recognition : currentRecognitions) {
-            double x = (recognition.getLeft() + recognition.getRight()) / 2 ;
-            double y = (recognition.getTop()  + recognition.getBottom()) / 2 ;
+        for (recognition in currentRecognitions) {
+            val x = ((recognition.left + recognition.right) / 2).toDouble()
+            val y = ((recognition.top + recognition.bottom) / 2).toDouble()
+            telemetry.addData("", " ")
+            telemetry.addData("Image", "%s (%.0f %% Conf.)", recognition.label, recognition.confidence * 100)
+            telemetry.addData("- Position", "%.0f / %.0f", x, y)
+            telemetry.addData("- Size", "%.0f x %.0f", recognition.width, recognition.height)
+        } // end for() loop
+    } // end method telemetryTfod()
 
-            telemetry.addData(""," ");
-            telemetry.addData("Image", "%s (%.0f %% Conf.)", recognition.getLabel(), recognition.getConfidence() * 100);
-            telemetry.addData("- Position", "%.0f / %.0f", x, y);
-            telemetry.addData("- Size", "%.0f x %.0f", recognition.getWidth(), recognition.getHeight());
-        }   // end for() loop
-
-    }   // end method telemetryTfod()
-
-}   // end class
+    companion object {
+        private const val USE_WEBCAM = true // true for webcam, false for phone camera
+    }
+} // end class
