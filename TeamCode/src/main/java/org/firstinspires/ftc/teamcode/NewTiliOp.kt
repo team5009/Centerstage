0e5d7d4b41de34551489160c0c49c8981768ef17
Team 5009 <team5009@centrenord.ab.ca>
2023-11-29 17:20:53 -0700

    small change for TeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewTiliOp.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewTiliOp.kt
@@ -43,6 +43,8 @@ class NewTiliOp : LinearOpMode() {
 
             if (gamepad2.dpad_down) {
                 bot.bot.intake.power = 0.8
+
+                
                 bot.bot.flap.power = -1.0
                 telemetry.addLine("ABSORBING :O")
             } else if (gamepad2.dpad_up) {
